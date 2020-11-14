244e30dd295a1b3a8b9788362137e423d0194d57
Marta Font <martafontb@gmail.com>
2020-11-14 14:57:43 +0100

    page transitions added

--- js/4thkind.js
+++ js/4thkind.js
@@ -1,7 +1,62 @@
-const mqDark = window.matchMedia("(prefers-color-scheme: dark)")
 
-// const darkModeToggle = document.querySelector("a.dark-mode-toggle")
-// const darkModeToggleText = darkModeToggle.querySelector("span")
+//Barba transitions
+const photoPage = document.querySelector('section.photo')
+const contactPage = document.querySelector('section.contact')
+
+
+barba.use(barbaCss)
+
+barba.init({
+  debug: true,
+  transitions: [
+    {
+      name: "fade",
+      once() {
+
+      },
+      beforeEnter({ current, next, trigger }) {
+        const href = next.url.path
+        const headerLinks = document.querySelectorAll("header a")
+
+        headerLinks.forEach(tag => {
+          if (tag.getAttribute("href") === href) {
+            tag.classList.add("selected")
+          } else {
+            tag.classList.remove("selected")
+          }
+        }),
+
+        window.scrollTo({
+          top: 0,
+          behavior: "smooth"
+        })
+      }
+    }
+  ],
+
+  views: [{
+    namespace: 'photo',
+    beforeEnter() {
+      photoPage.classList.add("photo")
+    },
+    beforeLeave(){
+      photoPage.classList.remove("photo")
+    }
+  }, {
+    namespace: 'contact',
+    beforeEnter() {
+      contactPage.classList.add("contact")
+    },
+    beforeLeave(){
+      contactPage.classList.remove("contact")
+    }
+  }]
+});
+
+
+
+
+const mqDark = window.matchMedia("(prefers-color-scheme: dark)")
 
 const menuToggle = document.querySelector("a.menu-toggle")
 // const menuToggleText = menuToggle.querySelector("span")
@@ -21,64 +76,3 @@ menuToggle.addEventListener("click", function () {
     gsap.to(".burger-mid", { width: 28 })
   }
 })
-
-// darkModeToggle.addEventListener("click", function () {
-//   if (bodyTag.classList.contains("dark-mode")) {
-//     darkModeToggleText.innerHTML = "Light mode"
-//     gsap.to("g.toggle", { x: 43 })
-//   } else {
-//     darkModeToggleText.innerHTML = "Dark mode"
-//     gsap.to("g.toggle", { x: 19 })
-//   }
-//
-//   const timeline = gsap.timeline()
-//
-//   timeline
-//   	.set("div.wipe", { height: "0%", top: "0%" })
-//   	.to("div.wipe", { height: "100%", duration: 2 })
-//   	.add(function () {
-//   		  bodyTag.classList.toggle("dark-mode")
-// 	 	})
-//   	.to("div.wipe", { height: "0%", top: "100%", duration: 2 })
-// })
-
-
-// const updateDarkMode = function () {
-//   if (mqDark.matches) {
-//     bodyTag.classList.add("dark-mode")
-//   	darkModeToggleText.innerHTML = "Light mode"
-//     gsap.to("g.toggle", { x: 43 })
-// 	} else {
-//     bodyTag.classList.remove("dark-mode")
-//     darkModeToggleText.innerHTML = "Dark mode"
-//     gsap.to("g.toggle", { x: 19 })
-//   }
-// }
-//
-// updateDarkMode()
-// mqDark.addListener(function () {
-//   updateDarkMode()
-// })
-//
-//
-// const spiralTimeline = gsap.timeline({
-//   repeat: -1
-// })
-//
-// spiralTimeline
-// 	.set("svg.spiral rect", {
-//   	rotation: 0,
-//   	transformOrigin: "50% 50%"
-// 	})
-// 	.set("svg.spiral rect:nth-child(1)", {
-//   	rotation: 15
-// 	})
-// 	.set("svg.spiral rect:nth-child(3)", {
-//   	rotation: -15
-// 	})
-// 	.to("svg.spiral rect", {
-//   	rotation: "+=90",
-//   	transformOrigin: "50% 50%",
-//   	duration: 4,
-//   	stagger: -0.25
-// 	})
