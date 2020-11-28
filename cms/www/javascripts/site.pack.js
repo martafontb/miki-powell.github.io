700b904cdf7ee1446c1d074eeb57a92d23823248
Marta Font <martafontb@gmail.com>
2020-11-28 21:59:59 +0100

    cms added

---
+++ cms/www/javascripts/site.pack.js
@@ -0,0 +1,15 @@
+// This is a main JavaScript file that will ll be compiled into /javascripts/site.js
+//
+// Any JavaScript file in your site folder can be referenced here by using import or require statements.
+// Additionally, you can import modules installed via your package.json file.
+//
+// For example:
+// import './fileName'
+//
+// To learn more, visit https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/import
+
+import './animate.js'
+import './gsap.min.js'
+import './jquery.js'
+import './modal.js'
+import './script.js'
